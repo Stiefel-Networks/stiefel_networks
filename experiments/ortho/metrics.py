a726b53352e36adbedb26f9c9095b977f1350dd1
Kirby Banman <kirby.banman@gmail.com>
2019-10-28 21:43:14 -0600

    improve documentation

--- experiments/ortho/metrics.py
+++ experiments/ortho/metrics.py
@@ -5,11 +5,11 @@ from ortho.util import normalize_columns
 
 def cosine_similarity_matrix(matrix):
     """
-
     :param matrix:  Any square NxN matrix.
-    :return: A matrix describing column-wise orthogonality of the input matrix.
-             Specifically, element i,j is the cosine similarity of the columns
-             i and j of the input matrix.
+    :return:
+        A matrix describing column-wise orthogonality of the input matrix.
+        Specifically, element i,j is the cosine similarity of the columns
+        i and j of the input matrix.
     """
     # We only care about the angles between the columns, not their magnitudes.
     # So normalize all of the columns.
@@ -26,13 +26,14 @@ def cosine_similarity_matrix(matrix):
 def total_cosine_simiarity(matrix):
     """
     :param matrix: Any square NxN matrix.
-    :return: A scalar describing column-wise orthogonality of the input matrix.
-             Specifically, return the total cosine similarity between the column
-             vectors of the matrix.  A matrix where all columns are pairwise
-             orthogonal has a total cosine similarity of 0.  A matrix where all
-             columns exist along the same line (i.e. rank == 1) has a total
-             cosine similarity of N * (N - 1) / 2, which is simply the number
-             of elements above (or below) the main diagonal of the input.
+    :return:
+        A scalar describing column-wise orthogonality of the input matrix.
+        Specifically, return the total cosine similarity between the column
+        vectors of the matrix.  A matrix where all columns are pairwise
+        orthogonal has a total cosine similarity of 0.  A matrix where all
+        columns exist along the same line (i.e. rank == 1) has a total
+        cosine similarity of N * (N - 1) / 2, which is simply the number
+        of elements above (or below) the main diagonal of the input.
     """
     cosine_similarities = cosine_similarity_matrix(matrix)
 
@@ -48,10 +49,37 @@ def total_cosine_simiarity(matrix):
 def mean_cosine_similarity(matrix):
     """
     :param matrix: Any square NxN matrix
-    :return: Same as total_cosine similarity, but normalized by the number of
-             unique, non-self pairs of columns in the matrix: N * (N - 1) / 2
-             An orthogonal matrix has mean cosine similarity of 0, and a matrix
-             with rank 1 has mean cosine similarity of 1.
+    :return:
+        Same as total_cosine similarity, but normalized by the number of
+        unique, non-self pairs of columns in the matrix: N * (N - 1) / 2
+        An orthogonal matrix has mean cosine similarity of 0, and a matrix
+        with rank 1 has mean cosine similarity of 1.
+
+        For example, the following matrix
+
+        [[ 1, 0, 0 ]
+         [ 0, 2, 0 ]
+         [ 0, 0, 3 ]]
+
+        has mean cosine similarity 1.0, because all columns are orthogonal.
+        Compare to the matrix
+
+        [[ 1, 0, 0 ]
+         [ 2, 0, 0 ]
+         [ 3, 0, 0 ]]
+
+        which has mean cosine similarity 0.0, because all columns are
+        collinear.  Compare again to
+
+        [[ 1, 0 ]
+         [ 1, 1 ]]
+
+        which has mean cosine similarity 0.7071.  There is only one cosine
+        similarity in the matrix - the one between column 1 and column 2.
+        They are 45 degrees apart, meaning cosine similarity of 0.7071.
+
+    :WARNING:
+        This currently explodes for any matrix with a column of zeros.
     """
     matrix_size = matrix.shape[0]
     number_of_non_self_pairs = matrix_size * (matrix_size - 1) / 2
