c855adb582b1519e956ed474390bcb0bedee2439
Alex Ayoub <43243047+aa14k@users.noreply.github.com>
2019-11-01 13:57:41 -0600

    Update util.py

--- experiments/ortho/util.py
+++ experiments/ortho/util.py
@@ -6,16 +6,17 @@ def random_square_matrix(matrix_size):
         .randn(matrix_size ** 2)\
         .reshape((matrix_size, matrix_size))
 
-def dependent_square_matrix(n,delta):
-    vec = np.zeros(n*n)
-    test = np.random.uniform(0,1,size=n*n)
-    sampleInit = np.random.uniform(0,1,size=n*n)
-    vec[0] = np.random.uniform(0,1)
+#Created By Alex, this function creates a dependent random matrix
+def dependent_square_matrix(n,delta):#takes in n(size of square matrix) and delta(percent of elements in matrix that are dependent
+    vec = np.zeros(n*n)#initializes our random dependent matrix
+    test = np.random.uniform(0,1,size=n*n)#stores our test values for deciding which elements should be dependent
+    sampleInit = np.random.uniform(0,1,size=n*n)#stores our random matrix values
+    vec[0] = np.random.uniform(0,1)#initializes the first element of our random matrix
     for i in range(1,n*n):
-        if test[i] <= delta:
-            vec[i] = vec[i-1]
-        else:
-            vec[i] = sampleInit[i]
+        if test[i] <= delta: #if our random test value is less than delta then
+            vec[i] = vec[i-1] #make the current element of the random matrix the previous element
+        else: 
+            vec[i] = sampleInit[i] #set the current element of the random matrix to a independent random number
     randomMat = vec.reshape((n,n))
     return randomMat
 
