df6bd4a775eafae3905f1bdcd0bf5db3b3f9a68a
Kirby Banman <kirby.banman@gmail.com>
2019-12-04 00:55:02 -0700

    finish experimental setup, ready to start running

--- experiments/mnist_F_SVD/f_svd.py
+++ experiments/mnist_F_SVD/f_svd.py
@@ -5,22 +5,28 @@ from tools.neur.module import SVDLinear
 
 class FSVD(nn.Module):
 
-    def __init__(self, input_dim, output_dim, layer_count=4, layer_width=128, bias=True):
+    def __init__(self, input_dim, output_dim, layer_count=4, layer_width=128, bias=True, batch_norm=True):
         super().__init__()
+        self.linearities = []
 
         self.first = SVDLinear(input_dim, layer_width)
+        self.linearities.append(self.first)
 
         hidden = []
         for _layer in range(layer_count):
-            hidden.append(SVDLinear(layer_width, layer_width, bias))
-            hidden.append(nn.ReLU())
+            svd_linearity = SVDLinear(layer_width, layer_width, bias)
+            hidden.append(svd_linearity)
+            self.linearities.append(svd_linearity)
 
+            hidden.append(nn.ReLU())
             # TODO: are we sure we should be batchnorming?
-            hidden.append(nn.BatchNorm1d(layer_width))
+            if batch_norm:
+                hidden.append(nn.BatchNorm1d(layer_width))
 
         self.hidden = nn.Sequential(*hidden)
 
         self.last = SVDLinear(layer_width, output_dim)
+        self.linearities.append(self.last)
 
     def forward(self, inputs):
         out = self.first(inputs)
@@ -30,3 +36,6 @@ class FSVD(nn.Module):
 
         # TODO: this might be the right place to construct a softmax, but not right now!
         return out
+
+    def singular_value_sets(self):
+        return [linearity.Sweight for linearity in self.linearities]
--- experiments/mnist_F_SVD/run.py
+++ experiments/mnist_F_SVD/run.py
@@ -15,30 +15,68 @@ from tools.util import get_gpu
 OUT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'runs')
 
 
-def record_shared(progress, run_start, epoch, samples_trained):
+def get_loss(labels, predictions):
+    return F.cross_entropy(predictions, labels)
+
+
+def get_accuracy(inputs, labels, predictions):
+    return (torch.argmax(predictions, dim=1) == labels).sum() * 100 / inputs.shape[0]
+
+
+def evaluate_model(f_svd_net, data_set):
+    total_samples = 0
+    total_loss = 0
+    total_accuracy = 0
+    for inputs, labels in data_set:
+        total_samples += len(inputs)
+
+        predictions = f_svd_net(inputs.reshape(-1, 100))
+        loss = get_loss(labels, predictions)
+        accuracy = get_accuracy(inputs, labels, predictions)
+
+        total_loss += len(inputs) * loss.item()
+        total_accuracy += len(inputs) * accuracy.item()
+
+    return total_loss / total_samples, total_accuracy / total_samples
+
+
+def record_shared(progress, run_start, epoch, samples_trained, f_svd_net):
     progress.update({
         'seconds_elapsed': (datetime.datetime.now() - run_start).seconds,
         'sample_exposure': samples_trained,
         'epoch': epoch,
+        'singular_values': [values.tolist() for values in f_svd_net.singular_value_sets()],
     })
 
 
 # args, run_start, epoch, batch, test_set, train_inputs, train_labels):
-def record_epoch(progress_array, run_start, epoch, samples_trained):
-    progress = {
+def record_epoch(progress_array, run_start, epoch, samples_trained, f_svd_net, use_gpu):
+    test_set = get_tiny_mnist_test(use_gpu=use_gpu)
+    train_set = get_tiny_mnist(batch_size=1000, use_gpu=use_gpu)
 
+    train_loss, train_accuracy = evaluate_model(f_svd_net, train_set)
+    test_loss, test_accuracy = evaluate_model(f_svd_net, test_set)
+
+    print("Train: {:.1f}% @ L={:.3f}".format(train_accuracy, train_loss))
+    print("Test:  {:.1f}% @ L={:.3f}".format(test_accuracy, test_loss))
+    progress = {
+        'train_loss': train_loss,
+        'train_accuracy': train_accuracy,
+        'test_loss': test_loss,
+        'test_accuracy': test_accuracy,
     }
-    record_shared(progress, run_start, epoch, samples_trained)
+    record_shared(progress, run_start, epoch, samples_trained, f_svd_net)
     progress_array.append(progress)
 
 
 # args, run_start, epoch, batch, batch_inputs, batch_labels, predictions):
-def record_batch(progress_array, run_start, batches_completed, epoch, samples_trained, batch_accuracy):
+def record_batch(progress_array, run_start, batches_completed, epoch, samples_trained, batch_accuracy, batch_loss, f_svd_net):
     progress = {
         'batches_completed': batches_completed,
         'batch_accuracy': batch_accuracy,
+        'bach_loss': batch_loss,
     }
-    record_shared(progress, run_start, epoch, samples_trained)
+    record_shared(progress, run_start, epoch, samples_trained, f_svd_net)
     progress_array.append(progress)
 
 
@@ -52,20 +90,21 @@ def run_epoch(args, batches_progress, epoch, f_svd_net, optimizer, run_start, sa
             optimizer.zero_grad()
 
             predictions = f_svd_net(inputs.reshape(-1, 100))
-            loss = F.cross_entropy(predictions, labels)
-
+            loss = get_loss(labels, predictions)
             loss.backward()
             optimizer.step()
 
+            # Record batch data
             samples_trained += len(inputs)
+            accuracy = get_accuracy(inputs, labels, predictions)
+            record_batch(batches_progress, run_start, batch, epoch, samples_trained, accuracy.item(), loss.item(), f_svd_net)
 
+            # Update progress bar
             previous_losses[batch % 10] = loss
             loss_window_average = previous_losses.mean().item()
-            accuracy = (torch.argmax(predictions, dim=1) == labels).sum() * 100 / inputs.shape[0]
             t.postfix = "{}% @ L={:.3f}  (L_avg={:.3f})".format(accuracy.item(), loss.item(), loss_window_average)
             t.update()
 
-            record_batch(batches_progress, run_start, batch, epoch, samples_trained, accuracy.item())
     return samples_trained
 
 
@@ -102,15 +141,14 @@ def main(hyperparams, run_name):
 
         optimizer = torch.optim.SGD(f_svd_net.parameters(), lr=args['learning_rate'], momentum=0.9)
 
-        test_set = get_tiny_mnist_test(use_gpu=args['use_gpu'])
         samples_trained = 0
         # Range and batch start at 1!
         epoch = 1
         for epoch in range(1, args['epochs'] + 1):
-            record_epoch(epochs_progress, run_start, epoch, samples_trained)
+            record_epoch(epochs_progress, run_start, epoch, samples_trained, f_svd_net, args['use_gpu'])
             samples_trained = run_epoch(args, batches_progress, epoch, f_svd_net, optimizer, run_start, samples_trained)
 
-        record_epoch(epochs_progress, run_start, epoch, samples_trained)
+        record_epoch(epochs_progress, run_start, epoch, samples_trained, f_svd_net, args['use_gpu'])
         save_run_data(db, run_name, run_number, run_start)
 
 
@@ -121,8 +159,7 @@ if __name__ == "__main__":
         'batch_size': 5000,
         'layer_width': 32,
         'learning_rate': 0.01,
-        'epochs': 1,
+        'epochs': 2,
         'num_runs': 2,
         'use_gpu': True,
     }, 'fast_test')
-# TODO: store train loss, train accuracy, test loss, test accuracy, singular values
