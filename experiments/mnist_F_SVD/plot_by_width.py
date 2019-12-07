2927c0eacd15ab4d8dbd9db4749d42f2bdef1dc3
Kirby Banman <kirby.banman@gmail.com>
2019-12-06 22:06:32 -0700

    finalize overparametrization plot

--- experiments/mnist_F_SVD/plot_by_width.py
+++ experiments/mnist_F_SVD/plot_by_width.py
@@ -2,21 +2,11 @@ import os
 import json
 import gc
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 from mnist_F_SVD.run import OUT_PATH
-from tools.measurement import stable_rank
-
-
-def get_architecture(run_data):
-    if run_data["hyperparameters"]["parametrization"] == "svd":
-        network_name = "F_SVD"
-    elif run_data["hyperparameters"]["parametrization"] == "standard":
-        network_name = "F_1"
-    else:
-        raise Exception("Unknown network parametrization {}".format(run_data["hyperparameters"]["parametrization"]))
-    return network_name
+from tools.plotting import get_architecture, get_run_files
 
 
 def plot_test_train_comparison(final_epochs, network_name, plot_directory=None):
@@ -26,10 +16,11 @@ def plot_test_train_comparison(final_epochs, network_name, plot_directory=None):
 
     fig = plt.figure(figsize=(6.4, 4.8))
     ax = fig.add_subplot(1, 1, 1)
+    ax.set_xticks([8, 16, 32, 64, 128, 256])
     ax.set_xlim((0, 256))
-    ax.set_ylim((0., 0.5))
+    ax.set_ylim((0., 0.2))
 
-    ax.plot(range(256), [0.1] * 256, label="Reference Line @ 0.1", linestyle='dotted', color='gray')
+    ax.axvline(64, label="N_T = 64", color='gray', linestyle='dashed', linewidth='1')
 
     for hyperparameter_key in final_epochs:
         x = final_epochs[hyperparameter_key]['layer_widths']
@@ -55,36 +46,27 @@ def plot_test_train_comparison(final_epochs, network_name, plot_directory=None):
 
 
 def main():
-    run_files = [f for f in os.listdir(OUT_PATH) if os.path.isfile(os.path.join(OUT_PATH, f))]
-    run_files.sort()
-
     final_epochs = {'F_1': {}, 'F_SVD': {}}
-    for run_filname in run_files:
-        with open(os.path.join(OUT_PATH, run_filname)) as file:
-            # These files can be many megabytes, up to a few gigabytes.  Let the garbage collector
-            # do its thing in advance of loading the next one.
-            gc.collect()
+    for run_data, run_filname in get_run_files(OUT_PATH):
+        # These files can be many megabytes, up to a few gigabytes.  Let the garbage collector
+        # do its thing in advance of loading the next one.
+        gc.collect()
 
-            print("Loading {}...".format(run_filname))
-            run_data = json.loads(file.read())
+        network_name = get_architecture(run_data)
+        hyperparameters = run_data["hyperparameters"]
+        hyperparameter_key = "b={} lr={} run {}".format(hyperparameters["batch_size"], hyperparameters["learning_rate"], hyperparameters["run_number"])
 
-            print("Loaded:\n  {}".format(run_data["hyperparameters"]))
+        if hyperparameter_key not in final_epochs[network_name]:
+            final_epochs[network_name][hyperparameter_key] = {
+                'layer_widths': [],
+                'train_losses': [],
+                'test_losses': [],
+            }
 
-            network_name = get_architecture(run_data)
-            hyperparameters = run_data["hyperparameters"]
-            hyperparameter_key = "b={} lr={} run {}".format(hyperparameters["batch_size"], hyperparameters["learning_rate"], hyperparameters["run_number"])
-
-            if hyperparameter_key not in final_epochs[network_name]:
-                final_epochs[network_name][hyperparameter_key] = {
-                    'layer_widths': [],
-                    'train_losses': [],
-                    'test_losses': [],
-                }
-
-            final_epoch = run_data["epochs_progress"][-1]
-            final_epochs[network_name][hyperparameter_key]['layer_widths'].append(hyperparameters["layer_width"])
-            final_epochs[network_name][hyperparameter_key]['train_losses'].append(final_epoch["train_loss"])
-            final_epochs[network_name][hyperparameter_key]['test_losses'].append(final_epoch["test_loss"])
+        run_last_epochs = run_data["epochs_progress"][-3:]
+        final_epochs[network_name][hyperparameter_key]['layer_widths'].append(hyperparameters["layer_width"])
+        final_epochs[network_name][hyperparameter_key]['train_losses'].append(np.mean([epoch["train_loss"] for epoch in run_last_epochs]))
+        final_epochs[network_name][hyperparameter_key]['test_losses'].append(np.mean([epoch["test_loss"] for epoch in run_last_epochs]))
 
     plot_directory = os.path.join(OUT_PATH, "cross_run")
     os.makedirs(plot_directory, exist_ok=True)
